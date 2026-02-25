818b22769090ac6a0134e447e88c33c008080b0c
mxxikr <mxxikr@gmail.com>
2026-02-25 19:15:40 +0900

    실무 활용 - 순수 JPA와 Querydsl: 조회 API 컨트롤러 개발

--- querydsl/src/main/java/study/querydsl/QuerydslApplication.java
+++ querydsl/src/main/java/study/querydsl/QuerydslApplication.java
@@ -13,8 +13,8 @@ public class QuerydslApplication {
 		SpringApplication.run(QuerydslApplication.class, args);
 	}
 
-	@Bean
-	JPAQueryFactory jpaQueryFactory(EntityManager em) {
-		return new JPAQueryFactory(em);
-	}
+//	@Bean
+//	JPAQueryFactory jpaQueryFactory(EntityManager em) {
+//		return new JPAQueryFactory(em);
+//	}
 }
